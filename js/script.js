489bdd2a9684ab6d74b6a10a3473d9e98a93dbbc
devkrytoy4el <50165574+devkrytoy4el@users.noreply.github.com>
2021-05-08 00:40:17 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,25 +3,27 @@ const menu = document.querySelector('.header');
 const mainContent = document.querySelector('.main__content')
 const nav = document.querySelector('.nav');
 const list = document.querySelector('.nav__list');
-const items = list.querySelectorAll('.nav__list-item');
 
 
 menuButton.addEventListener('click', function(event) {
-    menu.classList.add('active');
-    mainContent.classList.add('none');
-    nav.classList.add('active2');
-    list.classList.add('active3');
-    menuButton.classList.add('margin')
+    menu.classList.toggle('active');
+    mainContent.classList.toggle('none');
+    nav.classList.toggle('active2');
+    list.classList.toggle('active3');
+    menuButton.classList.toggle('margin');
+    let target = event.target;
+    console.log(target)
 })
 
 
 list.addEventListener('click', function(event) {
     let target = event.target;
+    console.log(target)
     if (target.className = 'nav__item') {
         menu.classList.remove('active');
         mainContent.classList.remove('none');
         nav.classList.remove('active2');
         list.classList.remove('active3');
-        menuButton.classList.remove('margin')
+        menuButton.classList.remove('margin');
     }
 })
